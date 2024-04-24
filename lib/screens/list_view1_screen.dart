73353d48cfb909a2b8f9cdb2f2878aba98b58350
jrms <email@fake.com>
2024-04-23 18:58:42 -0700

    Add ListTile to list view

--- lib/screens/list_view1_screen.dart
+++ lib/screens/list_view1_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ListView1Screen extends StatelessWidget {
+  final options = const ['One', 'Two', 'Three', 'Four', 'Five'];
   const ListView1Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('List View1 Screen')),
-        body: ListView(children: const [
-          Text('Item 1'),
-          Text('Item 2'),
+        body: ListView(children: [
+          ...options.map((e) => ListTile(
+                leading: const Icon(Icons.list),
+                title: Text(e),
+                trailing: const Icon(Icons.chevron_right),
+              ))
+
+          // ListTile(leading: Icon(Icons.list), title: Text('Item 1')),
         ]));
   }
 }
