56df51dab05b6648ca5079ebb7747e4b2b44ff08
jrms <email@fake.com>
2024-04-25 14:38:19 -0700

    Edit homeScreen>item builder's AppRoutes.menuOptions

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/router/app_routes.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,13 +12,17 @@ class HomeScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: ListView.separated(
-        itemBuilder: (BuildContext context, int index) {
-          return null;
-        },
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          leading: Icon(AppRoutes.menuOptions[index].icon),
+          title: Text(AppRoutes.menuOptions[index].name),
+          onTap: () {
+            Navigator.pushNamed(context, AppRoutes.menuOptions[index].route);
+          },
+        ),
         separatorBuilder: (BuildContext context, int index) {
           return const Divider();
         },
-        itemCount: 10,
+        itemCount: AppRoutes.menuOptions.length,
       ),
     );
   }
