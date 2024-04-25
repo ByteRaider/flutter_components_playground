15e92f692145e3c12453e4db197551a81e2f3f2f
jrms <email@fake.com>
2024-04-25 13:17:29 -0700

    Create Models and list routes

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,9 +1,36 @@
+import 'package:fl_components/models/models.dart';
 import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   // AppRoutes
   static const initialRoute = '/home';
+  static final menuOptions = <MenuOption>[
+    // menuOptions
+    MenuOption(
+      route: 'home',
+      name: 'Home Screen',
+      screen: const HomeScreen(),
+      icon: Icons.home,
+    ),
+    // MenuOption
+    MenuOption(
+      route: 'list_view1',
+      name: 'List View 1',
+      screen: const ListView1Screen(),
+      icon: Icons.list_alt,
+    ),
+    MenuOption(
+        route: 'list_view2',
+        name: 'List View 2',
+        screen: const ListView2Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'alert',
+        name: 'Alert',
+        screen: const AlertScreen(),
+        icon: Icons.alarm),
+  ];
   static Map<String, Widget Function(BuildContext)> routes = {
     // routes
     'home': (BuildContext context) => const HomeScreen(),
@@ -17,4 +44,4 @@ class AppRoutes {
     // onGenerateRoute
     return MaterialPageRoute(builder: (context) => const AlertScreen());
   } // onGenerateRoute
-}// AppRoutes
+} // AppRoutes
