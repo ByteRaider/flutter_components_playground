7bd141d4e01de120069f9f6990927045cd5f3d28
jrms <email@fake.com>
2024-04-26 16:37:54 -0700

    Create animated screen container

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -44,6 +44,12 @@ class AppRoutes {
         name: 'Avatar',
         screen: const AvatarScreen(),
         icon: Icons.supervised_user_circle_outlined),
+
+    MenuOption(
+        route: 'animated_container',
+        name: 'Animated Container',
+        screen: const AnimatedScreen(),
+        icon: Icons.play_circle_outline),
   ];
 
   // static Map<String, Widget Function(BuildContext)> routes = {
---
+++ lib/screens/animated_screen.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AnimatedScreen extends StatefulWidget {
+  const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.indigo;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(10);
+
+  void changeShape() {
+    print('Changing shape');
+    final random = Random();
+    _width = random.nextInt(300).toDouble() + 70;
+    _height = random.nextInt(300).toDouble() + 70;
+    _color = Color.fromARGB(
+        random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+    _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble() + 10);
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(borderRadius: _borderRadius, color: _color),
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => changeShape(),
+          backgroundColor: Colors.blue,
+          child: const Icon(
+            Icons.play_arrow,
+            size: 30,
+          ),
+        ));
+  }
+}
