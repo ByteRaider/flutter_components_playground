6428df2851222698300308f996c55a8db7d8b798
jrms <email@fake.com>
2024-04-26 23:55:44 -0700

    Add email and password fields

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -11,6 +11,28 @@ class InputScreen extends StatelessWidget {
         body: const SingleChildScrollView(
             child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Column(children: [CustomInputField()]))));
+                child: Column(children: [
+                  CustomInputField(
+                    hintText: 'Name',
+                    labelText: 'Full Name',
+                    helperText: 'Name with at least 6 characters',
+                    keyboardType: TextInputType.name,
+                  ),
+                  SizedBox(height: 30),
+                  CustomInputField(
+                    hintText: 'Email address',
+                    labelText: 'Email',
+                    helperText: 'Enter your email',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  CustomInputField(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    helperText: 'Enter your password',
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 30),
+                ]))));
   }
 }
